daa5d411b7d2fe900b94298380a86ee04135f7f9
mao710 <marcos.eter3913@gmail.com>
2023-06-08 14:21:43 -0300

    Finish assig 01

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
-    }
+        Console.WriteLine("Como estas?");
+        Console.Write("hola");
+        Console.Write("hello");
+        Console.Write("Gato");
+    } 
 }
