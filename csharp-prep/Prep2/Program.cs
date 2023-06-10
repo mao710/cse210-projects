8d1b97a1ec8774e2dc3c4415468f5dfc1de14b47
mao710 <marcos.eter3913@gmail.com>
2023-06-09 22:04:53 -0300

    Assignemnts2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+     
+     
+        Console.WriteLine("Which is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int percentage = int.Parse(percent);
+        string letter ="";
+
+        if (percentage >= 90)
+        {
+            letter= "A";
+            }
+
+        else if (percentage >= 80)
+        {   letter= "B";}
+    
+        else if (percentage >= 70)
+        {   letter= "C";}
+
+        else if (percentage >= 60)
+        {   letter= "D";}
+
+        else
+        {   letter="F";}
+
+        Console.Write($"Your grade is: {letter}. ");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+
+
+
     }
 }
